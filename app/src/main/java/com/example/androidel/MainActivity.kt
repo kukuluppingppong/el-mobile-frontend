da0c9129f7464eda8ce857a24e8c187a747c93bc
wlsdn9297 <wlsdn9297@naver.com>
2022-07-08 13:29:39 +0900

    기본 설정

--- app/src/main/java/com/example/androidel/MainActivity.kt
+++ app/src/main/java/com/example/androidel/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.androidel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
